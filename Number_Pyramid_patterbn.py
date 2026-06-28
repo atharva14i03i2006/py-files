b4b9aa653c8d419ab4ae36c6eadc524ba1348e3c
atharva14i03i2006 <atharva.sawant24@aiml.sce.edu.in>
2026-06-28 23:52:09 +0530

    Add Number Pyramid

---
+++ Number_Pyramid_patterbn.py
@@ -0,0 +1,16 @@
+class Pyramid():
+    n = int(input("Enter the number of rows: "))
+
+    for i in range(1, n + 1):
+
+        for j in range(1, n - i + 1):
+            print(" ", end="")
+
+        for j in range(1, i + 1):
+            print(j, end="")
+
+        for j in range(i - 1, 0, -1):
+            print(j, end="")
+
+        print()
+Pyramid()
