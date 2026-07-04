89c21c2ef758ae2782bea5dd4396aa13ffb57093
atharva14i03i2006 <atharva.sawant24@aiml.sce.edu.in>
2026-07-05 00:20:39 +0530

    Small change

--- Blood_relation.py
+++ Blood_relation.py
@@ -1,11 +1,10 @@
 user_input = input("Enter the relation : ")
 Gender = input(" Gender : ")
-if user_input == "Father" :
-    print("Father of the user")
-elif user_input == "Mother":
-    print("Mother of the user")
-elif user_input == "Brother":
-    print("Brother of the user")
-
-
-    
+if user_input == ("Father" or "mother") and Gender == "M":
+    print("son")
+elif user_input == ("Mother"or "Father") and Gender == "F":
+    print("daughter")
+elif user_input == ("Brother" or "Sister") and Gender == "M":
+    print("brother")
+elif user_input == ("Brother" or "Sister") and Gender == "F":
+    print("sister")
