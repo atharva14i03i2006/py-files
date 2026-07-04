80a40f24a7bd132a7182bc2497aa3f3238a9e4e6
atharva14i03i2006 <atharva.sawant24@aiml.sce.edu.in>
2026-07-04 23:27:52 +0530

    Blood Relation simple program

---
+++ Blood_relation.py
@@ -0,0 +1,11 @@
+user_input = input("Enter the relation : ")
+Gender = input(" Gender : ")
+if user_input == "Father" :
+    print("Father of the user")
+elif user_input == "Mother":
+    print("Mother of the user")
+elif user_input == "Brother":
+    print("Brother of the user")
+
+
+    
