dde9cf2709aef00731a17eadca4c3d4e506bcaf8
atharva14i03i2006 <atharva.sawant24@aiml.sce.edu.in>
2026-07-07 17:52:57 +0530

    Light change

---
+++ Prefect_Number.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(1, n):
+    if n % i == 0:
+        sum += i
+
+if sum == n:
+    print(n, "is a Perfect Number")
+else:
+    print(n, "is not a Perfect Number")
