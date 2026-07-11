bd9e0cc4f8214fbd2ba2296f1712d3d88c732d87
atharva14i03i2006 <atharva.sawant24@aiml.sce.edu.in>
2026-07-11 23:47:20 +0530

    Recursive Function

---
+++ Function Logics/Perfect_Number_using_function.py
@@ -0,0 +1,15 @@
+def perfect(n):
+    s = 0
+
+    for i in range(1, n):
+        if n % i == 0:
+            s += i
+
+    return s == n
+
+num = int(input("Enter number: "))
+
+if perfect(num):
+    print("Perfect Number")
+else:
+    print("Not Perfect")
---
+++ Function Logics/Recursive_Fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input("Enter term: "))
+
+for i in range(n):
+    print(fibonacci(i), end=" ")
+    
