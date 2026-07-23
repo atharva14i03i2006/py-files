30e85243a737a128a8d1f550cc3a9ef26b987891
Atharva Sawant <atharva.sawant24@aiml.sce.edu.in>
2026-07-23 16:04:57 +0530

    Final Boss
    Fix syntax errors and improve print statements for matrix traversal.

--- Final Boss/Snake_pattern_matrix.py
+++ Final Boss/Snake_pattern_matrix.py
@@ -6,6 +6,8 @@ n = int(input("Enter a number of rows : /n")
 A = [list(map(int , input("Enter a matrix :").split())) for _ in range(n)]
 for i in range(1,n+1):
     if i % 2 == 0:
-       print("Left to right")
+       for j in range(n):
+         print(A[i][j], end=" ")
     else:
-       print("Right to left")
+       for j in range(n - 1, -1 , -1):
+         print(A[i][j], end=" ")
