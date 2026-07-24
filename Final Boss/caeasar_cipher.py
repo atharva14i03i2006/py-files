24d89eb022fe5e8e481357279850cc36c91a7bdf
Atharva Sawant <atharva.sawant24@aiml.sce.edu.in>
2026-07-24 18:28:21 +0530

    Implement Caesar cipher for string encryption

--- Final Boss/caeasar_cipher.py
+++ Final Boss/caeasar_cipher.py
@@ -1,1 +1,20 @@
+'''
+caeasar_cipher
+'''
+s = input("Enter a string: ")
+n = int(input("Enter the shift value: "))
 
+result = ""
+n = n % 26  
+
+for ch in s:
+    if ch.isupper():
+        new_ch = chr((ord(ch) - ord('A') + n) % 26 + ord('A'))
+        result += new_ch
+    elif ch.islower():
+        new_ch = chr((ord(ch) - ord('a') + n) % 26 + ord('a'))
+        result += new_ch
+    else:
+        result += ch
+
+print("Encrypted text:", result)
